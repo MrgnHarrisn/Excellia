d365748d7bf6afa1d3ce19335fc814883adb03a3
Morgan Harrison <63645728+MrgnHarrisn@users.noreply.github.com>
2023-12-13 22:18:31 +1300

    Fixed creating image on render

--- Excellia/Excellia/WorldManager.cpp
+++ Excellia/Excellia/WorldManager.cpp
@@ -2,7 +2,7 @@
 
 WorldManager::WorldManager()
 {
-
+	m_image.create(m_width, m_height, sf::Color::Black);
 }
 
 void WorldManager::create()
@@ -13,6 +13,17 @@ void WorldManager::create()
 	min.x = 0, min.y = 0;
 	max.x = 800, max.y = 800;
 	m_caves = TerrainGeneration::generate_caves(min, max, m_random);
+
+	for (int i = 0; i < m_heights.size(); i++) {
+		/* Do dirt for column */
+		for (int j = m_heights[i]; j <= m_heights[i] + m_dirt_heights[i]; j++) {
+			m_image.setPixel(i, j, Color(150, 75, 0));
+		}
+		for (int j = 799; j > m_heights[i] + m_dirt_heights[i]; j--) {
+			m_image.setPixel(i, j, sf::Color(100, 100, 100));
+		}
+	}
+
 }
 
 void WorldManager::create(long int seed)
@@ -23,25 +34,26 @@ void WorldManager::create(long int seed)
 	min.x = 0, min.y = 0;
 	max.x = 800, max.y = 800;
 	m_caves = TerrainGeneration::generate_caves(min, max, seed);
-}
-
-sf::Sprite WorldManager::get_render(RenderWindow& w)
-{
-	sf::Image image;
-	image.create(m_width, m_height, sf::Color::Black);
 
 	for (int i = 0; i < m_heights.size(); i++) {
 		/* Do dirt for column */
 		for (int j = m_heights[i]; j <= m_heights[i] + m_dirt_heights[i]; j++) {
-			image.setPixel(i, j, Color(150, 75, 0));
+			m_image.setPixel(i, j, Color(150, 75, 0));
 		}
 		for (int j = 799; j > m_heights[i] + m_dirt_heights[i]; j--) {
-			image.setPixel(i, j, sf::Color(100, 100, 100));
+			m_image.setPixel(i, j, sf::Color(100, 100, 100));
 		}
 	}
 
+}
+
+sf::Sprite WorldManager::get_render(RenderWindow& w)
+{
+
+	
+
 	sf::Texture texture;
-	texture.loadFromImage(image);
+	texture.loadFromImage(m_image);
 	sf::Sprite sprite(texture);
 
 	w.draw(sprite);
