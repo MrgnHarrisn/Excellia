c1659c5c23e7c1e944879188ecfe5f803703ca6b
draknol <49766243+Draknol@users.noreply.github.com>
2023-12-16 21:32:50 +1300

    Collision seems to be working
    havent broken this version yet but probably will be

--- Excellia/Excellia/Player.cpp
+++ Excellia/Excellia/Player.cpp
@@ -34,23 +34,23 @@ void Player::update(float dt)
 
 	// Check Movement
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-		velocity.x += m_speed * dt;
+		velocity.x += m_speed;
 	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-		velocity.x -= m_speed * dt;
+		velocity.x -= m_speed;
 	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-		velocity.y -= m_speed * dt;
+		velocity.y -= m_speed;
 	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-		velocity.y += m_speed * dt;
+		velocity.y += m_speed;
 	}
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-        velocity.y -= (m_move_speed + 30) * dt;
+        velocity.y -= m_move_speed + 30;
 		
     }
 
@@ -59,12 +59,24 @@ void Player::update(float dt)
 	}
 
     // Temp gravity
-    velocity.y += 30 * dt;
+    velocity.y += 30;
 
-    // Call temp collision
+    // Call collision
     sf::Vector2f pos = get_position();
-	velocity = can_move_pos(pos, velocity);
-	set_position(pos + velocity);
+	sf::Vector2f temp_v = sf::Vector2f(velocity.x *= 0.0001, velocity.y *= 0.0001);
+	
+	while (dt > 0.0001)
+	{
+		pos += can_move_pos(pos, temp_v);
+		set_position(pos);
+		dt -= 0.0001;
+	}
+
+	velocity.x *= dt;
+	velocity.y *= dt;
+
+	pos += can_move_pos(pos, velocity);
+	set_position(pos);
 
     // Update shape position
 	m_shape.setPosition(get_position());
@@ -79,84 +91,83 @@ sf::Sprite Player::get_sprite()
 
 sf::Vector2f Player::can_move_pos(sf::Vector2f &position, sf::Vector2f velocity)
 {
-	sf::Vector2f new_pos_x = get_position() + sf::Vector2f(velocity.x, 0);
-	sf::Vector2f new_pos_y = get_position() + sf::Vector2f(0, velocity.y);
+	const float e = 0.0001;
 
 	/* For X */
 	if (velocity.x < 0) {
-		if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(0, 0.001)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
+		if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(0, 0.01)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
 			velocity.x = 0;
-			position.x = static_cast<int>(position.x);
+			position.x = static_cast<int>(position.x + 0.5-e);
 		}
 		else if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(0, 1)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
 			velocity.x = 0;
-			position.x = static_cast<int>(position.x);
+			position.x = static_cast<int>(position.x + 0.5-e);
 		}
 		else if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(0, 2)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
 			velocity.x = 0;
-			position.x = static_cast<int>(position.x);
+			position.x = static_cast<int>(position.x + 0.5-e);
 		}
-		else if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(0, 2.999)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
+		else if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(0, 3-e)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
 			velocity.x = 0;
-			position.x = static_cast<int>(position.x);
+			position.x = static_cast<int>(position.x + 0.5-e);
 		}
 	}
 	else if (velocity.x > 0) {
-		if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(-1, 0.001)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
+		if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(-1, 0.01)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
 			velocity.x = 0;
-			position.x = static_cast<int>(position.x + 0.5);
+			position.x = static_cast<int>(position.x + 0.5-e);
 		}
 		else if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(-1, 1)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
 			velocity.x = 0;
-			position.x = static_cast<int>(position.x + 0.5);
+			position.x = static_cast<int>(position.x + 0.5-e);
 		}
 		else if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(-1, 2)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
 			velocity.x = 0;
-			position.x = static_cast<int>(position.x + 0.5);
+			position.x = static_cast<int>(position.x + 0.5-e);
 		}
-		else if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(-1, 2.999)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
+		else if (m_wm.get_block((sf::Vector2i)((get_position() - sf::Vector2f(-1, 3-e)) + sf::Vector2f(velocity.x, 0))) != Block::Void) {
 			velocity.x = 0;
-			position.x = static_cast<int>(position.x + 0.5);
+			position.x = static_cast<int>(position.x + 0.5-e);
 		}
 	}
 
 	/* For Y*/
 	if (velocity.y > 0)
 	{
-		if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(-0.001, 0) + sf::Vector2f(0, velocity.y))) != Block::Void) {
+		if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(0-e, 0) + sf::Vector2f(0, velocity.y))) != Block::Void) {
 			velocity.y = 0;
-			position.y = static_cast<int>(position.y + 0.5) - 0.001;
+			position.y = static_cast<int>(position.y + 0.5-e) - e;
 		}
-		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(-0.999, 0) + sf::Vector2f(0, velocity.y))) != Block::Void) {
+		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(e-1, 0) + sf::Vector2f(0, velocity.y))) != Block::Void) {
 			velocity.y = 0;
-			position.y = static_cast<int>(position.y + 0.5) - 0.001;
+			position.y = static_cast<int>(position.y + 0.5-e) - e;
 		}
-		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(-0.001, 2.999) + sf::Vector2f(0, velocity.y))) != Block::Void) {
+		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(0-e, 3-e) + sf::Vector2f(0, velocity.y))) != Block::Void) {
 			velocity.y = 0;
-			position.y = static_cast<int>(position.y + 0.5) - 0.001;
+			position.y = static_cast<int>(position.y + 0.5-e) - e;
 		}
-		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(-0.999, 2.999) + sf::Vector2f(0, velocity.y))) != Block::Void) {
+		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(e-1, 3-e) + sf::Vector2f(0, velocity.y))) != Block::Void) {
 			velocity.y = 0;
-			position.y = static_cast<int>(position.y + 0.5) - 0.001;
+			position.y = static_cast<int>(position.y + 0.5-e) - e;
 		}
 	}
 	else if (velocity.y < 0)
 	{
-		if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(-0.001, 0.001) + sf::Vector2f(0, velocity.y))) != Block::Void) {
+		if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(0-e, e) + sf::Vector2f(0, velocity.y))) != Block::Void) {
 			velocity.y = 0;
-			position.y = static_cast<int>(position.y + 0.5);
+			position.y = static_cast<int>(position.y + 0.5-e);
 		}
-		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(-0.999, 0.001) + sf::Vector2f(0, velocity.y))) != Block::Void) {
+		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(e-1, e) + sf::Vector2f(0, velocity.y))) != Block::Void) {
 			velocity.y = 0;
-			position.y = static_cast<int>(position.y + 0.5);
+			position.y = static_cast<int>(position.y + 0.5-e);
 		}
-		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(-0.001, 2.999) + sf::Vector2f(0, velocity.y))) != Block::Void) {
+		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(0-e, 3-e) + sf::Vector2f(0, velocity.y))) != Block::Void) {
 			velocity.y = 0;
-			position.y = static_cast<int>(position.y + 0.5);
+			position.y = static_cast<int>(position.y + 0.5-e);
 		}
-		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(-0.999, 2.999) + sf::Vector2f(0, velocity.y))) != Block::Void) {
+		else if (m_wm.get_block((sf::Vector2i)(get_position() - sf::Vector2f(e-1, 3-e) + sf::Vector2f(0, velocity.y))) != Block::Void) {
 			velocity.y = 0;
-			position.y = static_cast<int>(position.y + 0.5);
+			position.y = static_cast<int>(position.y + 0.5-e);
 		}
 	}
 
--- Excellia/Excellia/main.cpp
+++ Excellia/Excellia/main.cpp
@@ -27,11 +27,10 @@ int main()
 
 	// Creates window
 	sf::RenderWindow window(sf::VideoMode(settings.get_screen_size().x, settings.get_screen_size().y), "Pixellia", sf::Style::None);
-	// window.setFramerateLimit(60);
+	window.setFramerateLimit(10);
+
 	/* 573849 test seed */
-	/* Crashes with 83875675 */
 	/* 42069 is a good seed */
-	/* Spawning inside dirt 6911 */
 
 	// Creates world
 	WorldManager wm(window, settings.get_world_size(), 573849);
