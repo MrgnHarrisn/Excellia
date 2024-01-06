19f27ef69ae5d2aef005a9e366aec77e30987d1a
Morgan Harrison <63645728+MrgnHarrisn@users.noreply.github.com>
2024-01-06 22:09:20 +1300

    Better jump physics

--- Excellia/Excellia/Player.h
+++ Excellia/Excellia/Player.h
@@ -55,11 +55,11 @@ private:
 	float m_slow_move_speed = 4;
 	float m_sprint_speed = 25;
 	float m_speed = m_move_speed;
-	float m_jump_force = 15;
+	float m_jump_force = 30;
 	sf::Texture m_texture;
 	sf::Sprite m_sprite;
 	WorldManager& m_wm;
-	float gravity = 44.9;
+	float gravity = 120;
 	bool m_can_jump = true;
 	float m_jump_timer = 0.0f;
 	sf::Vector2f m_velocity{ 0,0 };
